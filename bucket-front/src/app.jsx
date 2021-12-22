82e38ed055b1a41b70f79c2ac3c022e736ae7179
seolyu ♪ *҉＼( ˆᴗˆ )ﾉ✩ <seolyu.tjfdb@gmail.com>
2021-12-22 21:28:25 +0900

    front | DeathMode Toggle 퍼블리싱

--- bucket-front/src/app.jsx
+++ bucket-front/src/app.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./app.css";
 import Intro from "./pages/Intro";
 import Header from "./components/Header";
+import DeathMode from "./components/DeathMode";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Intro />} />
       </Routes>
+      <DeathMode />
     </BrowserRouter>
   );
 }
---
+++ bucket-front/src/components/DeathMode.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const DeathMode = () => {
+  return (
+    <div className="DeathMode">
+      <label className="mode">
+        <input type="checkbox" />
+        <span className="slider round"></span>
+      </label>
+    </div>
+  );
+};
+
+export default DeathMode;
